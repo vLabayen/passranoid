f9a9e82032fa26ad060d2684f4c93fb3944fa23e
vLabayen <victor.mendi3216@gmail.com>
2019-10-13 22:25:03 +0200

    readme, folders and termux installation

--- passranoid.sh
+++ passranoid.sh
@@ -1,5 +1,5 @@
-export PYTHONPATH="/media/windows/proyects/passranoid/v9/modules"
-database="/media/windows/proyects/passranoid/v9/databases/pass.db"
-privkey="/media/windows/proyects/passranoid/v9/keys/pass_rsa"
-pubkey="/media/windows/proyects/passranoid/v9/keys/pass_rsa.pub"
+export PYTHONPATH="/<path>/passranoid/modules"
+database="/<path>/passranoid/databases/pass.db"
+privkey="/<path>/passranoid/keys/pass_rsa"
+pubkey="/<path>/passranoid/keys/pass_rsa.pub"
 python3 script/passranoid.py $database $privkey $pubkey $@
